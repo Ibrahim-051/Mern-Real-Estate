0b477163cb904091e836a6c94effdc1244db4e6e
Ibrahim-051 <ibrahim.jutt017@gmail.com>
2025-04-28 22:18:35 +0500

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className=" flex justify-center items-center w-full h-40 text-3xl text-center bg-blue-300 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
